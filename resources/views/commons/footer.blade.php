83e0e95b8c5349f12f1a6a4cf7e464f8d0cd7cdc
Juri-O <ju121.prog.acc.0925@gmail.com>
2020-11-04 13:14:20 +0000

    Laravel課題（応用）１３

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="mt-5">
+    
+    <nav class="navbar navbar-light bg-light justify-content-center">
+        
+        <div class="navbar-brand m-0">©︎2020 QuestAcademia,All rights reserved. </div>
+        
+    </nav>
+    
+</footer>
